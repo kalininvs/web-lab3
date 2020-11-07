9e59f6f97b68a4c5641b00224b4068eee9e19b7c
pro100autist <kalininvs@inbox.ru>
2020-11-07 16:58:24 +0300

    edit view, SVG

--- js/pyatnashkiView.js
+++ js/pyatnashkiView.js
@@ -7,7 +7,7 @@ var Scene = document.getElementById("mainScene");
 let animTime;
 var View = function(func1) {
 	document.addEventListener("DOMContentLoaded", function() {  func1();  } );
-	canvas = null;
+	svg = null;
 	cellsize = 0;
 	movingX = 1;
 	movingY = 1;
@@ -49,6 +49,7 @@ View.prototype.getNullCell = function(arr){
 	}
 }
 View.prototype.animated = function(model) {
+	
 	animation = true;
 	var Empty = this.getNullCell(model.arr); 
 	if(Object.keys(model.pos).length == 0) { return;}
@@ -57,7 +58,6 @@ View.prototype.animated = function(model) {
 		
 		this.checkPosition(model,Empty,model.pos);
 	}.bind(this), 50);
-
 }
 View.prototype.setPosition = function(empty,pos) {
 	pos1.x = (empty.y*cellsize); pos1.y = (empty.x*cellsize); 
@@ -79,37 +79,48 @@ View.prototype.checkPosition = function(model,empty,pos){
 		this.tableFill(model);
 }
 View.prototype.initTable = function(model,onKeyDownEvent){
-	canvas = document.createElement("canvas");
-	canvas.id = "canvas";
-	canvas.width = 252;
-	canvas.height = 252;
-	cellsize = canvas.width / 4;
-	var ctx = canvas.getContext("2d");
-	this.context = ctx;
+	svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
+	svg.setAttributeNS('http://www.w3.org/2000/svg','xlink','http://www.w3.org/1999/xlink');
+	svg.setAttribute ("width", "252" );
+	svg.setAttribute ("height", "252" );
+	svg.setAttribute ("id", "svg" );
+	cellsize = svg.getAttribute("width") / 4;
 	for(i = 0; i < 4; ++i){
 		for(j = 0; j < 4; ++j){
-			this.cellView(j*cellsize,i*cellsize,cellsize);
-			this.context.beginPath();
-			this.numView();
-			this.context.fillText(
-				model.arr[i][j], 
-				j * cellsize + cellsize / 2,
-				i * cellsize + cellsize / 2
-			);
-			this.context.fill();
+			svg.appendChild(this.getNode('rect', {id:i+""+j , x: j*cellsize+1, y:i*cellsize+1, width:cellsize-2, height:cellsize-2, fill:'#00FFFF' }));
+			svg.appendChild(this.setText('text',model.arr[i][j], {x: (j * cellsize + cellsize / 2)-5, y: (i * cellsize + cellsize / 2)+5, fill:'red' }));
 		}		
 	}
 	if(Scene.childNodes.length >= 1)
     Scene.removeChild(Scene.firstChild);	
-	Scene.appendChild(canvas);
-	document.getElementById('canvas').addEventListener("click", function(e) {
+	Scene.appendChild(svg);
+	document.getElementById('svg').addEventListener("click", function(e) {
 		var mouse = {
 			x:  Math.trunc((e.clientX - Scene.offsetLeft) / 63),
 			y: Math.trunc((e.clientY - Scene.offsetLeft) / 63)
 		}
 		onKeyDownEvent(mouse);  } );
 }
-View.prototype.cellAnimated = function(text,pos){
+View.prototype.getNode = function(n, v) {
+	n = document.createElementNS('http://www.w3.org/2000/svg', n);
+	for (var p in v)
+	  n.setAttributeNS(null, p, v[p]);
+	return n
+}
+View.prototype.setText = function(n,text, v) {
+	n = document.createElementNS('http://www.w3.org/2000/svg', n);
+	n.setAttributeNS(null,'font-family','Avenir, Helvetica, sans-serif');
+	n.setAttributeNS(null,'font-size',cellsize/3+"px");
+	n.setAttributeNS(null,'font-weight','bold');
+	n.innerHTML = text;
+	for (var p in v)
+	{
+		if(p=="x"&Number(text)>=10){ n.setAttributeNS(null, p,Number(v[p])-5 ); continue; }
+		n.setAttributeNS(null, p, v[p]);
+	}
+	return n
+}
+View.prototype.cellAnimated2 = function(text,pos){
 	this.cellView(pos.x,pos.y,cellsize);
 	this.context.beginPath();
 	this.numView();
@@ -120,11 +131,15 @@ View.prototype.cellAnimated = function(text,pos){
 	);
 	this.context.fill();
 }
+View.prototype.cellAnimated = function(text,pos){
+	svg.appendChild(this.getNode('rect', {id:pos.x+""+pos.y , x: pos.x+1, y:pos.y+1, width:cellsize-2, height:cellsize-2, fill:'#00FFFF' }));
+	svg.appendChild(this.setText('text',text, {x: (pos.x+ cellsize / 2)-5, y: (pos.y + cellsize / 2)+5, fill:'red' }));
+}
 View.prototype.tableFill = function(model){
+	while (svg.lastChild) {
+		svg.removeChild(svg.lastChild);
+	}
 	var Empty = animation? this.getNullCell(model.arr) : 0;
-	var ctx = document.getElementById('canvas').getContext("2d");
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	this.context = ctx;
 	this.cellAnimated(model.arr[Empty.x][Empty.y],pos2);
 	this.cellAnimated(model.arr[model.pos.x][model.pos.y],pos1);
 	for(i = 0; i < 4; ++i){
@@ -133,15 +148,17 @@ View.prototype.tableFill = function(model){
 			{
 				continue;
 			}
-			this.cellView(j*cellsize,i*cellsize,cellsize);
-			this.context.beginPath();
-			this.numView();
-			this.context.fillText(
-				model.arr[i][j], 
-				j * cellsize + cellsize / 2,
-				i * cellsize + cellsize / 2
-			);
-			this.context.fill();
+			svg.appendChild(this.getNode('rect', {id:i+""+j , x: j*cellsize+1, y:i*cellsize+1, width:cellsize-2, height:cellsize-2, fill:'#00FFFF' }));
+			svg.appendChild(this.setText('text',model.arr[i][j], {x: (j * cellsize + cellsize / 2)-5, y: (i * cellsize + cellsize / 2)+5, fill:'red' }));
+			// this.cellView(j*cellsize,i*cellsize,cellsize);
+			// this.context.beginPath();
+			// this.numView();
+			// this.context.fillText(
+			// 	model.arr[i][j], 
+			// 	j * cellsize + cellsize / 2,
+			// 	i * cellsize + cellsize / 2
+			// );
+			// this.context.fill();
 		}		
 	}
 	if(this.checkAnimation()) { return; }
